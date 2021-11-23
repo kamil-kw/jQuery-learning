599b9b9e9daf70bb3b8fbb17bbebc0a2b4365f9b
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-23 20:26:22 +0000

    jQuery event

--- 03-jQueryEvents/02-events_in_jquery/video_solution/script.js
+++ 03-jQueryEvents/02-events_in_jquery/video_solution/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+    $("#stream1_btn").mouseleave(function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
