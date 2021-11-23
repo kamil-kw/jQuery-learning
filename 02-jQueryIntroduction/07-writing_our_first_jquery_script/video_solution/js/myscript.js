3c066021076642032cb1d35f33d1e3e02ec4445a
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-23 20:09:03 +0000

    Manipulating DOM

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
@@ -1,3 +1,13 @@
+// $(document).ready(function() {
+//     $("h2").css("text-decoration", "underline"); //underline all <h2>
+//     $("ul").css("border", "solid 1px #ccc"); //adds a border to all list
+// });
+
+// $(document).ready(function() {
+//     $("h2").addClass("underline"); //underline all <h2>
+//     $("ul").addClass("border"); //adds a border to all list
+// });
+
 $(document).ready(function() {
-    
+    $
 });
