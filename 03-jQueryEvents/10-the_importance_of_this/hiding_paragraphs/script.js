9bc8af0cffc55a5a1369f09633e7a899b7cf9890
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-24 12:01:52 +0000

    Learninig this

--- 03-jQueryEvents/10-the_importance_of_this/hiding_paragraphs/script.js
+++ 03-jQueryEvents/10-the_importance_of_this/hiding_paragraphs/script.js
@@ -1,3 +1,40 @@
-$('p').click(function() {
-	$(this).slideToggle('slow');
+$('button').mouseenter(function() {
+	$(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function() {
+	$(this).removeClass('makeBlue').addClass('makeRed');
+});
+
+// boxes
+
+// $(document).ready(function(){
+// 	$(".box").on("click", function() {
+// 		var classNames = $(this).attr("class").split(" ");
+// 		$("." + classNames[1]).css("background-color", "red");
+// 	});
+// });
+
+$(document).ready(function(){
+	$(".box").on("click", function() {
+		/**
+		 * When we click on an element that has
+		 * a "box" class, this event will be fired.
+		 */
+		var classNames = $(this).attr("class").split(" ");
+		var boxClass = classNames[0];
+		var className = classNames[1];
+		if ($(this).css("background-color") === "rgb(255, 0, 0)") {
+			// If this object is already red, turn it black
+			$("." + className).css("background-color", "#000");
+		
+		} else {
+			// Else turn all elements with a box class black
+			// and then change the color of all elements with 
+			// the same className as the clicked element to red.
+		$("." + boxClass).css("background-color", "#000");
+		$("." + className).css("background-color", "red");
+		}
+
+	});
 });
